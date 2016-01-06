8847105f13a6c1832be142546c1b0da55624040b
Thomas Devos <devos.thom@gmail;com>
2016-01-06 11:30:25 +0100

    Added: Menu

--- js/page-scroll.js
+++ js/page-scroll.js
@@ -1,12 +1,52 @@
 $(function(){
     "use strict"; // Start of use strict
+
+    //var currentAnchor = function(){
+    //    var anchor = null;
+    //    $('section').each(function(){
+    //        var id = $(this).attr('id');
+    //        var position = $(this).offset().top - 100;
+    //        if(position > $(window).scrollTop()){
+    //            return anchor;
+    //        }
+    //        anchor = id;
+    //    });
+    //    return anchor;
+    //};
+
     $('a').on('click', function (event) {
         var $anchor = $(this);
+        if($anchor.attr('href') == '#'){
+            var top = 0;
+        } else {
+            var top = $($anchor.attr('href')).offset().top - 50;
+        }
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
+            scrollTop: (top)
+        }, 1250
+            //,
+            //function(){
+            //    console.log(currentAnchor());
+            //}
+        );
         event.preventDefault();
     });
 
     new WOW().init();
+
+    $(window).scroll(function(){
+        onScroll();
+    });
+    onScroll();
+
+
 });
+
+var onScroll = function(){
+    if($(window).scrollTop() > 0){
+        $('nav').addClass('scrolled');
+    } else {
+        $('nav').removeClass('scrolled');
+    }
+};
+
